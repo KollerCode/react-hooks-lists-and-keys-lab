948b2a2a3c049cf112d1844ff152e5ffd4cee892
Natasha Koller <kollernatasha@gmail.com>
2022-02-04 20:23:42 -0500

    added nav bar 2 options

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,29 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  // const linkElements = links.map((link) => {
+  //   return (
+  //     <div>
+  //       <a href="#home" key={link}>{link}</a>
+  //     <a href="#about" key={link.about}>About</a>
+  //     <a href="#projects" key={link.projects}>Projects</a>
+  //     </div>
+  //   );
+  // });
+
+  return (
+    <nav>
+      <a href="#home" key={links.home}>
+        home
+      </a>
+      <a href="#about" key={links.about}>
+        About
+      </a>
+      <a href="#projects" key={links.projects}>
+        Projects
+      </a>
+    </nav>
+  );
 }
 
 export default NavBar;
